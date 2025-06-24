ae8fe0b9509b9250da52b51391f2797a6a0ab88f
Jackson Kenneth Seifferly <jseifferly@meta.com>
2025-06-24 12:40:38 -0700

    [frontend][Header.jsx] Added a profile photo location for users to access their profiles later

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,6 +4,7 @@ export default function Header() {
     return(
         <header>
             <img src="/logo.png" alt="Logo" className='logo'/>
+            <img src="/default_profile_pic.jpg" alt="Profile Phot" className='profile-photo'/>
         </header>
     );
 }
