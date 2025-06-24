9642e8ea376404cad658aab3ed854d1b1cbc753a
Jackson Kenneth Seifferly <jseifferly@meta.com>
2025-06-23 17:16:08 -0700

    [frontend][main.jsx] Added react router to start laying out web pages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,15 @@
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router'
 import './index.css'
 import App from './App.jsx'
 
+const routes = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />
+    }
+])
+
 createRoot(document.getElementById('root')).render(
-    <App />
+    <RouterProvider router={routes} />
 )
