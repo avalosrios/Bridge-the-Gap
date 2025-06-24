a447b0ac3a493f06488a3a86934158c71fc2ae06
Jackson Kenneth Seifferly <jseifferly@meta.com>
2025-06-24 12:33:16 -0700

    [frontend][Header.jsx] Updated logo in header for cleaner header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,8 +3,7 @@ import '../styles/Header.css'
 export default function Header() {
     return(
         <header>
-            <h1 className='app-name'>Bridge the Gap</h1>
-            <span className='logo'>Logo goes here</span>
+            <img src="/logo.png" alt="Logo" className='logo'/>
         </header>
     );
 }
