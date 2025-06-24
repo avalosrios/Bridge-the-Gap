eeeb4c4c673b9200cb94f54d924458cc0d56f9a1
Jackson Kenneth Seifferly <jseifferly@meta.com>
2025-06-24 09:31:02 -0700

    Added stubs for endpoints for group routing

--- backend/api/server.js
+++ backend/api/server.js
@@ -4,7 +4,7 @@ const session = require('express-session')
 const server = express()
 server.use(express.json())
 
-// -----------------------USER ROUTING------------------------ //
+// ------------------------USER ROUTING------------------------- //
 
 //[GET] /api/user/?name
 //  Use prisma db commands to request for users based on usernames
@@ -22,7 +22,27 @@ server.delete('/api/user', async (req, res, next) => [
     console.log('DELETE request for user')
 ])
 
-// ---------------------------------------------------------- //
+// ------------------------------------------------------------ //
+
+// -----------------------GROUP ROUTING------------------------ //
+
+server.get('api/groups', async (req, res, next) => {
+    console.log("GET request for groups")
+})
+
+server.put('api/groups', async (req, res, next) => {
+    console.log("PUT request for groups")
+})
+
+server.post('api/groups', async (req, res, next) => {
+    console.log("POST request for groups")
+})
+
+server.delete('api/groups', async (req, res, next) => {
+    console.log("DELETE request for groups")
+})
+
+// ------------------------------------------------------------ //
 
 // [CATCH-ALL]
 server.use((req, res, next) => {
