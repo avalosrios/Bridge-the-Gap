12cf6b65a1a9be02c4e9d8eea9a3d7b6afd47bb4
Jackson Kenneth Seifferly <jseifferly@meta.com>
2025-06-25 14:39:42 -0700

    Added profile icon to the profile page as well as a back arrow to navigate back to home page

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,10 +1,27 @@
 import './ProfilePage.css'
+import { Link } from 'react-router';
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import ProfileBanner from '../components/ProfileBanner';
+import MemberIcon from '../components/MemberIcon'
+
+const defaultMember = {
+    name: "user",
+    profile_img: "/default_profile_pic.jpg"
+}
 
 function ProfilePage() {
     return (
-        <>
-        <h1>PROFILE PAGE</h1>
-        </>  
+        <div>
+        <Header />
+        <ProfileBanner />
+        <Link to='/' className="back-button">{'<--'}</Link>
+        <MemberIcon member={defaultMember} className="member-icon"/>
+        {
+            //Profile info
+        }
+        <Footer />
+        </div>  
     );
 }
 
