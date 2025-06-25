60495efc25f57691807394348125a84c15417384
Jackson Kenneth Seifferly <jseifferly@meta.com>
2025-06-25 09:44:29 -0700

    Updated Logo/title

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Bridge-the-Gap
 
-## By: Jackson Seifferly
+**By: Jackson Seifferly**
 
-## Meta University Internship Program 2025
+**Meta University Internship Program 2025**
 
 Project Plan: https://docs.google.com/document/d/1vNQNzT8_fTWdJuXJ9tGQTQziGGsSEb8sm781ow9e8tE/edit?usp=sharing
 
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,7 +3,9 @@ import '../styles/Header.css'
 export default function Header() {
     return(
         <header>
-            <img src="/logo.png" alt="Logo" className='logo'/>
+            <div className='logo'>
+                <h1>Bridge the Gap</h1>
+            </div>
             <img src="/default_profile_pic.jpg" alt="Profile Phot" className='profile-photo'/>
         </header>
     );
