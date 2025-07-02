b6f8f5a7216edb5532f107ffd9c9ca9b71f6c30b
Jackson Kenneth Seifferly <jseifferly@meta.com>
2025-07-02 16:03:59 -0700

    Fixed login  bug that would cause groups to mapped while it was still null

--- frontend/src/components/GroupList.jsx
+++ frontend/src/components/GroupList.jsx
@@ -3,7 +3,10 @@ import GroupCard from "./GroupCard";
 import useUserGroups from "../hooks/useUserGroups.js";
 
 export default function GroupList({ onOpen }) {
-  const { groups } = useUserGroups();
+  const { groups, isLoading } = useUserGroups();
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
 
   return (
     <section className="group-list">
