f63c22c5805157e7f3a0765b68c5fa8d9e13b9f1
Jackson Kenneth Seifferly <jseifferly@meta.com>
2025-07-02 15:58:38 -0700

    Users can now edit their profile on the profile page

--- frontend/src/components/ProfileBanner.jsx
+++ frontend/src/components/ProfileBanner.jsx
@@ -1,9 +1,12 @@
 import "../styles/ProfileBanner.css";
+import useUser from "../hooks/useUser.js";
 
 export default function ProfileBanner() {
+  const { user } = useUser();
+
   return (
     <div className="profile-banner">
-      <h2>Hello User!</h2>
+      <h2>{`Hello ${user.username}!`}</h2>
     </div>
   );
 }
